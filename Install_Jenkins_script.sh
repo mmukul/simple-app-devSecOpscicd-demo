c934c79dc1c1477880fceb28e98bccd5ccd36427
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-08 12:01:52 +0530

    Update Install_Jenkins_script.sh

--- Install_Jenkins_script.sh
+++ Install_Jenkins_script.sh
@@ -5,7 +5,7 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && \
 
-sudo yum install -y fontconfig java-11-openjdk && \
+sudo yum install -y git fontconfig java-11-openjdk && \
 sudo yum install -y jenkins && \
  
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp && \
